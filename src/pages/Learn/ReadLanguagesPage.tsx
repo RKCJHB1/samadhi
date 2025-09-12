443cca44e96dbdf7b7e884b4fdfd6febd600ad21
RKCJHB1 <jlvidyashala@gmail.com>
2025-09-12 22:56:10 +0200

    Update ReadLanguagesPage.tsx

--- src/pages/Learn/ReadLanguagesPage.tsx
+++ src/pages/Learn/ReadLanguagesPage.tsx
@@ -54,7 +54,7 @@ const ReadLanguagesPage: React.FC = () => {
     return () => { cancelled = true; };
   }, []);
 
-  const supConfigured = Boolean(remoteStats);
+  const supConfigured = isSupabaseConfigured();
 
 
 
